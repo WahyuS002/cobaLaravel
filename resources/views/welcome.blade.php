15ceb7478ffb5147c84271991a688c903b27c2b4
WahyuS002 <syahputrawahyu61@gmail.com>
2020-01-27 19:32:10 +0700

    Belajar Laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.main')
+
+@section('title', 'Welcome')
+
+@section('container')
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1 class="mt-3">Hello</h1>
+        </div>
+    </div>
+</div>
+@endsection
